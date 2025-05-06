8b77c6470989e1220b5bac493321b96535d67af3
Fadhli Firdausi <fadsayma@gmail.com>
2025-05-06 17:46:15 +0700

    Menambahkan navigasi ke page Momentum

--- app/src/main/java/com/fadhlifirdausi607062300117/asesment1/navigation/NavGraph.kt
+++ app/src/main/java/com/fadhlifirdausi607062300117/asesment1/navigation/NavGraph.kt
@@ -5,25 +5,52 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.NavType
 import com.fadhlifirdausi607062300117.asesment1.ui.screen.HelpScreen
 import com.fadhlifirdausi607062300117.asesment1.ui.screen.HydrationScreen
+import com.fadhlifirdausi607062300117.asesment1.ui.screen.NotesScreen
 import com.fadhlifirdausi607062300117.asesment1.ui.screen.MainScreen
+import com.fadhlifirdausi607062300117.asesment1.ui.screen.NotesDetailScreen
 import com.fadhlifirdausi607062300117.asesment1.ui.screen.SettingScreen
+import com.fadhlifirdausi607062300117.asesment1.ui.screen.KEY_NOTES_ID
 
 @Composable
-fun SetUpNavGraph(navController: NavHostController=rememberNavController()){
-    NavHost(navController = navController, startDestination = Screen.Home.route){
-        composable(route = Screen.Home.route){
+fun SetUpNavGraph(navController: NavHostController = rememberNavController()) {
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
+
+        composable(route = Screen.Home.route) {
             MainScreen(navController)
         }
-        composable(route = Screen.Help.route){
+
+        composable(route = Screen.Help.route) {
             HelpScreen(navController)
         }
+
         composable(route = Screen.Setting.route) {
             SettingScreen(navController)
         }
+
         composable(route = Screen.Hydration.route) {
             HydrationScreen(navController)
+        }
+
+        composable(route = Screen.Notes.route) {
+            NotesScreen(navController)
+        }
+
+        // Untuk menambahkan catatan baru
+        composable(route = Screen.NotesBaru.route) {
+            NotesDetailScreen(navController)
+        }
+
+        // Untuk melihat/mengedit catatan berdasarkan ID
+        composable(
+            route = Screen.NotesDetail.route,
+            arguments = listOf(navArgument(KEY_NOTES_ID) { type = NavType.LongType })
+        ) { navBackStackEntry ->
+            val id = navBackStackEntry.arguments?.getLong(KEY_NOTES_ID)
+            NotesDetailScreen(navController, id)
         }
     }
 }
--- app/src/main/java/com/fadhlifirdausi607062300117/asesment1/navigation/Screen.kt
+++ app/src/main/java/com/fadhlifirdausi607062300117/asesment1/navigation/Screen.kt
@@ -1,12 +1,21 @@
 package com.fadhlifirdausi607062300117.asesment1.navigation
 
-sealed class Screen (val route : String){
+import com.fadhlifirdausi607062300117.asesment1.ui.screen.KEY_NOTES_ID
+
+sealed class Screen(val route: String) {
     data object Home : Screen("mainScreen")
     data object Profile : Screen("profileScreen")
     data object Setting : Screen("settingScreen")
     data object Help : Screen("helpScreen")
     data object Hydration : Screen("hydrationScreen")
+    data object Notes : Screen("notesScreen")
 
+    // Route untuk menambahkan catatan baru (tanpa parameter)
+    data object NotesBaru : Screen("notesDetailScreen")
 
+    // Route untuk mengedit/melihat catatan berdasarkan ID
+    data object NotesDetail : Screen("notesDetailScreen/{$KEY_NOTES_ID}"){
+        fun withId(id:Long) = "notesDetailScreen/$id"
 
+    }
 }
