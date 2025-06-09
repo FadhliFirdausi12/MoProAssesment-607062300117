cfcfa07d9edae1ae4b8e44fd6d0f0afa0805f26e
Fadhli Firdausi <fadsayma@gmail.com>
2025-06-09 11:08:41 +0700

    Migrasi API dari static ke dynamic

--- app/src/main/java/com/fadhlifirdausi607062300117/asesment1/network/RecipesApiService.kt
+++ app/src/main/java/com/fadhlifirdausi607062300117/asesment1/network/RecipesApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val  BASE_URL = "https://raw.githubusercontent.com/indraazimi/mobpro1-compose/static-api/"
+private const val BASE_URL = "https://gh.d3ifcool.org/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -19,18 +19,59 @@ private val retrofit = Retrofit.Builder()
     .build()
 
 
-interface RecipesApiService {
-    @GET("static-api.json")
+interface HewanApiService {
+    @GET("hewan.php")
     suspend fun getRecipes(): List<Recipes>
 }
 
 object RecipesApi{
-    val service: RecipesApiService by lazy{
-        retrofit.create(RecipesApiService::class.java)
+    val service: HewanApiService by lazy{
+        retrofit.create(HewanApiService::class.java)
     }
     fun getRecipesUrl(imageId: String): String{
-        return "$BASE_URL$imageId.jpg"
+        return "${BASE_URL}image.php?id=$imageId"
     }
 }
 
 enum class ApiStatus {LOADING, SUCCES, FAILED}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//modul 12 reference static API
+//private const val  BASE_URL = "https://raw.githubusercontent.com/indraazimi/mobpro1-compose/static-api/"
+//
+//private val moshi = Moshi.Builder()
+//    .add(KotlinJsonAdapterFactory())
+//    .build()
+//
+//private val retrofit = Retrofit.Builder()
+//    .addConverterFactory(MoshiConverterFactory.create(moshi))
+//    .baseUrl(BASE_URL)
+//    .build()
+//
+//
+//interface RecipesApiService {
+//    @GET("static-api.json")
+//    suspend fun getRecipes(): List<Recipes>
+//}
+//
+//object RecipesApi{
+//    val service: RecipesApiService by lazy{
+//        retrofit.create(RecipesApiService::class.java)
+//    }
+//    fun getRecipesUrl(imageId: String): String{
+//        return "$BASE_URL$imageId.jpg"
+//    }
+//}
+//
+//enum class ApiStatus {LOADING, SUCCES, FAILED}
